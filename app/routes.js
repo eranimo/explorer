9494929efdb47522e5779f22df68ce3700c52f1e
Kaelan Cooter <dcgamers@gmail.com>
2016-04-25 18:50:37 -0400

    removed home page

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
 import MapPage from './containers/MapPage';
 import StatsPage from './containers/StatsPage';
 
@@ -11,8 +10,7 @@ function NoMatch () {
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
-    <Route path="map" component={MapPage} />
+    <IndexRoute component={MapPage} />
     <Route path="stats" component={StatsPage} />
     <Route path="*" component={NoMatch}/>
   </Route>
